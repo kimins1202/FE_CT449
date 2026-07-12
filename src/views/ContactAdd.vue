3e4ae6f5f0f739d364929e87ae83215454d0cb71
kimins1202 <nguyenkimyen120205@gmail.com>
2026-07-12 14:11:23 +0700

    Them chuc nang them moi lien he

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="contact" class="page">
+    <h4>Thêm Liên hệ</h4>
+    <ContactForm :contact="contact" @submit:contact="createContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    ContactForm,
+  },
+  data() {
+    return {
+      contact: {
+        name: "",
+        email: "",
+        address: "",
+        phone: "",
+        favorite: false,
+      },
+      message: "",
+    };
+  },
+  methods: {
+    async createContact(data) {
+      try {
+        await ContactService.create(data);
+        alert("Liên hệ được thêm thành công.");
+        this.$router.push({ name: "contactbook" });
+      } catch (error) {
+        console.log(error);
+        this.message = "Không thể thêm Liên hệ. Vui lòng thử lại.";
+      }
+    },
+  },
+};
+</script>
